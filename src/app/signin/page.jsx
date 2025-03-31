3196b803ad80934d6508bb62c949d07647de528e
John Duavis <paulduavis09@gmail.com>
2025-03-31 22:47:38 +0800

    Fix folder structure and Sign in page

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <div>
+       <div className='grid grid-cols-[545px_1fr] h-dvh'>
+         <div className='bg-pink-200'>
+              <h1 className='text-4xl font-bold'>Welcome to SocialApp</h1>
+         </div>
+         <div className='bg-purple-200'>
+                <h1 className='text-4xl font-bold'>Sign In</h1>
+         </div>
+       </div>
+    </div>
+  )
+}
