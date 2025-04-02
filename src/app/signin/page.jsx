c66af6ee9001f1d5a9fc071fb54ab8dd465a8cdc
John Duavis <paulduavis09@gmail.com>
2025-04-02 21:30:07 +0800

    fix minor bug in signin and signup

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -3,6 +3,7 @@ import { FaGithub, FaGoogle } from 'react-icons/fa'
 
 
 export default function SignUp() {
+export default function SignUp() {
   return (
     <div className='h-screen'>
       <div className=' md:justify-start w-full md: p-0 grid md:grid-cols-[1fr_2fr] grid-cols-1'>
@@ -16,8 +17,9 @@ export default function SignUp() {
       </div>
      
 
-      <div className='grid md:grid-cols-[1fr_2fr] grid-cols-1 h-full' >
+      <div className='grid md:grid-cols-[1fr_2fr] grid-cols-1 h-full'>
         {/* Left: Sign-in Form */}
+        <div className='bg-neutral-900 flex flex-col px-6 py-6 md:px-23 md:py-3 border-b md:border-r border-neutral-700 '>
         <div className='bg-neutral-900 flex flex-col px-6 py-6 md:px-23 md:py-3 border-b md:border-r border-neutral-700 '>
           <p className='text-3xl font-light text-white mb-3'>Welcome back</p>
           <p className='text-gray-400 mb-8 text-[12px] font-semibold'>Sign in to your account</p>
@@ -26,9 +28,10 @@ export default function SignUp() {
             <FaGithub className='text-xl' /> Continue with GitHub
           </button>
           
-          <button className='w-full bg-neutral-800 border border-neutral-700 text-white py-3 rounded-lg flex items-center justify-center gap-3 mb-4 text-sm font-semibold hover:border-neutral-500 transition-all duration-200'>
+          <button className='w-full bg-neutral-800 border border-neutral-600 text-white py-3 rounded-lg flex items-center justify-center gap-3 mb-4 text-sm font-semibold'>
             <FaGoogle className='text-xl' /> Continue with Google
           </button>
+
 
           <div className='w-full flex items-center my-6'>
             <div className='flex-grow border-t border-gray-700'></div>
@@ -42,6 +45,7 @@ export default function SignUp() {
             className='w-full p-[10px] bg-neutral-800 rounded-lg bg-gray-850 text-white border border-gray-700 mb-4 text-xs' 
           />
           <div className='flex'>
+          <div className='flex'>
             <div className='text-[13px] pb-2 opacity-60'>Password</div>
             <div className='w-full text-right text-gray-400 text-[13px] opacity-50'>Forgot Password?</div>
           </div> 
@@ -53,13 +57,15 @@ export default function SignUp() {
 
           <button className='w-full bg-green-800 text-white py-2 rounded-lg text-lg mb-6 mt-7 border border-green-600 text-[15px] font-extralight hover:bg-green-700 transition-all duration-200'>Sign In</button>
           <div className='flex flex-col-2 justify-center'>
-            <p className='text-gray-400 text-xs text-center pr-1 '>Don't have an account? </p>
-            <span className='text-white cursor-pointert text-center underline text-sm hover:text-neutral-400'>Sign Up Now </span>
+            <p className='text-gray-400 text-xs text-center pr-1 '>Have an account? </p>
+            <span className='text-white cursor-pointert text-center underline text-sm'>Sign In Now </span>
              
           </div>
         </div>
 
         {/* Right: Quote Section */}
+        <div className='flex flex-col justify-center items-center text-white w-full h-full md:h-screen  md: px-6'>
+          <p className='text-2xl md:text-4xl italic text-gray-300 pb-6 p-6 md:p-30 leading-relaxed text-justify'>
         <div className='flex flex-col justify-center items-center text-white w-full h-full md:h-screen  md: px-6'>
           <p className='text-2xl md:text-4xl italic text-gray-300 pb-6 p-6 md:p-30 leading-relaxed text-justify'>
             “Social media isn’t just about posts—it’s about real connections, creativity, and community. Share, inspire, and grow together!”
