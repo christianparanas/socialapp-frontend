b2cec42ab12341e1c75de4af0d5c2e83a0b1501f
John Duavis <paulduavis09@gmail.com>
2025-04-04 19:48:09 +0800

    landing navigation bar

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -3,40 +3,45 @@ import React from 'react'
 function page() {
   return (
     <div> {/* Global*/}
-      <div className='flex justify-between border border-amber-200'>  {/* nav*/}
-        <div className='border border-b-blue-500 p-4 m-4'> {/* left nav*/}
-            <ul className='flex'>
-                <li className='flex'>
-                    <img src='/assets/images/logo.jpg' alt='Logo' className='w-13 h-13 rounded-full object-cover p-1 m-1' />
-                    <p>Social app</p>
+      <div className='flex justify-between border-b border-neutral-800 pl-40 pr-40 h-16'>  {/* nav*/}
+        <div className=''> {/* left nav*/}
+            <ul className='flex flex-wrap'>
+                <li className='flex h-16'>
+                    <img src='/assets/images/logo.jpg' alt='Logo' className='w-13 h-13 rounded-full object-cover p-2 mt-0.5' />
+                    <p className='p-4 font-bold'>Social app</p>
                 </li>
-                <li className='pl-2'>
-                    <p className='text-md font-semibold text-white mb-3'>Product</p>
+                <li className='pl-2 p-4'>
+                    <p className='text-sm font-semibold text-white pt-1 hover:text-green-600 transition-all duration-100'>Product</p>
                 </li>
-                <li className='pl-2'>
-                    <p className='text-md font-light text-white mb-3'>Developers</p>
+                <li className='pl-2 p-4'>
+                    <p className='text-sm font-semibold text-white pt-1 hover:text-green-600 transition-all duration-100'>Developers</p>
                 </li>
-                <li className='pl-2'>
-                    <p className='text-md font-light text-white mb-3'>Enterprise</p>
+                <li className='pl-2 p-4'>
+                    <p className='text-sm font-semibold text-white pt-1 hover:text-green-600 transition-all duration-100'>Enterprise</p>
                 </li>
-                <li className='pl-2'>
-                    <p className='text-md font-light text-white mb-3'>Pricing</p>
+                <li className='pl-2 p-4'>
+                    <p className='text-sm font-semibold text-white pt-1 hover:text-green-600 transition-all duration-100'>Pricing</p>
                 </li>
-                <li className='pl-2'>
-                    <p className='text-md font-light text-white mb-3'>Docs</p>
+                <li className='pl-2 p-4'>
+                    <p className='text-sm font-semibold text-white pt-1 hover:text-green-600 transition-all duration-100'>Docs</p>
                 </li>
-                <li className='pl-2'>
-                    <p className='text-md font-light text-white mb-3'>Blog</p>
+                <li className='pl-2 p-4'>
+                    <p className='text-sm font-semibold text-white pt-1 hover:text-green-600 transition-all duration-100'>Blog</p>
                 </li>
             </ul>
         </div>
-        <div className='border border-y-teal-500 p-4 m-4'> {/* right nav*/}
-            <ul className='flex'>
-                <li>
-                    <p className='text-md font-light text-white mb-3'>Sign In</p>
+        <div className=''> {/* right nav*/}
+            <ul className='flex flex-wrap'>
+                <li className='pl-4  pt-4'>
+                    <button className='bg-neutral-800 border border-neutral-600 text-white py-1 px-2 rounded-lg flex items-center justify-center gap-3 mb-4 text-sm font-semibold hover:bg-neutral-700 hover:border-neutral-500 transition-all duration-200'>
+                        <p className='text-xs font-extralight text-white '>Sign Up</p>
+                    </button>
+
                 </li>
-                <li>
-                    <p className='text-md font-light text-white mb-3'>Sign Up</p>
+                <li className='pl-2 p-4'>
+                    <button className='bg-green-800 border border-green-600 text-white py-1 px-2 rounded-lg flex items-center justify-center gap-3 mb-4 text-sm font-semibold hover:border-green-500 hover:bg-green-700 ease-in-out transition-all duration-200' >
+                        <p className='text-xs font-extralight text-white '>Sign In</p>
+                    </button>
                 </li>
             </ul>
         </div>
